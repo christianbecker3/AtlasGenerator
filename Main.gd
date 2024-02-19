5493beba3bbd77258f4d5b3b6fb8d78f1cafe0a8
Christian Becker (inf483) <christian.becker@smail.th-koeln.de>
2024-02-19 21:47:51 +0100

    Update 0.0.2
    - Resolution will be now set by the largest image available in the png sequence
    - Smaller images will be centered
    - Added an error exception, if the image can't be saved

--- Main.gd
+++ Main.gd
@@ -4,14 +4,9 @@ extends Control
 @onready var line_edit: LineEdit = %LineEdit
 @onready var file_dialog: FileDialog = %FileDialog
 @onready var horizontal_images: SpinBox = %HorizontalImages
-@onready var image_size: SpinBox = %Size
 @onready var square_image: CheckButton = %SquareImage
-@onready var info_label: Label = %Information
 var path: String
 var output_filename: String = "Atlas"
-
-func _ready() -> void:
-	update_info(0)
 
 func select_folder() -> void:
 	file_dialog.popup()
@@ -19,9 +14,6 @@ func select_folder() -> void:
 func setup_folder(input_path: String) -> void:
 	path = input_path
 	line_edit.text = path
-	
-func update_info(i: float) -> void:
-	info_label.text = "Atlas size: " + str(int(horizontal_images.value * image_size.value)) + " px"
 	
 func update_filename(new_text: String) -> void:
 	output_filename = new_text
@@ -38,18 +30,18 @@ func generate_atlas() -> void:
 	for file in files:
 		if file.get_extension() == "png" and file.get_slice(".", 0) != output_filename:
 			filtered_files.append(load_file(file, path))
+	var image_size: int = filter_largest_image_resolution_of_images(filtered_files).x
 	var x_size: int = horizontal_images.value
 	var y_size: int = ceil(float(filtered_files.size()) / float(x_size))
 	if filtered_files.size() < x_size:
 		show_popup("Please reduce the horizontal size to continue.")
 		return
-	var img_size: int = image_size.value
 	var is_square_image: bool = square_image.button_pressed
 	var atlas: Image = Image.new()
 	if is_square_image:
-		atlas = Image.create(x_size * img_size, x_size * img_size, true, Image.FORMAT_RGBA8)
+		atlas = Image.create(x_size * image_size, x_size * image_size, true, Image.FORMAT_RGBA8)
 	else:
-		atlas = Image.create(x_size * img_size, y_size * img_size, true, Image.FORMAT_RGBA8)
+		atlas = Image.create(x_size * image_size, y_size * image_size, true, Image.FORMAT_RGBA8)
 	var x_pos: int = -1
 	var y_pos: int = -1
 	for i in range(filtered_files.size()):
@@ -58,9 +50,13 @@ func generate_atlas() -> void:
 		if fmod(i, x_size) == 0:
 			y_pos += 1
 			x_pos = 0
-		var image_position: Vector2 = Vector2(x_pos * img_size, y_pos * img_size)
-		atlas.blit_rect(image, Rect2(Vector2.ZERO, Vector2(img_size, img_size)), image_position)
-	atlas.save_png(path + "\\" + output_filename + ".png")
+		var offset: Vector2i = Vector2i.ZERO
+		var center_anchor_position: Vector2i = (Vector2i(image_size, image_size) - image.get_size()) / 2
+		var image_position: Vector2i = Vector2i(x_pos * image_size + center_anchor_position.x, y_pos * image_size + center_anchor_position.y)
+		atlas.blit_rect(image, Rect2(Vector2i.ZERO, image.get_size()), image_position)
+	if atlas.save_png(path + "\\" + output_filename + ".png") != 0:
+		show_popup("An error occurred while saving the file.")
+		return
 	show_popup("Atlas was successfully created.")
 
 func load_file(filename: String, path: String) -> Image:
@@ -72,3 +68,10 @@ func load_file(filename: String, path: String) -> Image:
 func show_popup(text: String) -> void:
 	dialog_box.dialog_text = text
 	dialog_box.popup()
+
+func filter_largest_image_resolution_of_images(images: Array[Image]) -> Vector2i:
+	var resolution: Vector2i = Vector2i.ZERO
+	for image in images:
+		if resolution.length() < image.get_size().length():
+			resolution = image.get_size()
+	return resolution
